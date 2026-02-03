2ac8849458b54f24cf0e303fa173a6608d529bd3
abhiraj <abhiraj.bits@gmail.com>
2026-02-03 20:52:57 +0530

    Initial Commit

---
+++ accesso/accessoApp.swift
@@ -0,0 +1,17 @@
+//
+//  accessoApp.swift
+//  accesso
+//
+//  Created by KIET55 on 03/02/26.
+//
+
+import SwiftUI
+
+@main
+struct accessoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
